b5df729689e94431d28fbb2e60f46255edc85ddf
sajjad <titanxl79@gmail.com>
2023-05-08 18:22:53 +0430

    crod documents and properties parts finished

--- backend/urls.py
+++ backend/urls.py
@@ -1,6 +1,14 @@
 from django.contrib import admin
 from django.urls import path, include
+from rest_framework import routers
 from rest_framework_simplejwt import views as jwt_views
+from documentManagement.views import DocumentApi
+from propertyManagement.views import PersonApi, PropertyApi
+
+router = routers.DefaultRouter()
+router.register(r'documents', DocumentApi, 'documents')
+router.register(r'persons', PersonApi, 'persons')
+router.register(r'properties', PropertyApi, 'properties')
 
 urlpatterns = [
     path('admin/', admin.site.urls),
@@ -11,5 +19,5 @@ urlpatterns = [
          jwt_views.TokenRefreshView.as_view(),
          name='token_refresh'),
     path('', include('authentification.urls')),
-    path("", include("documentManagement.urls")),
+    path('api/', include(router.urls)),
 ]
--- documentManagement/urls.py
+++
@@ -1,7 +0,0 @@
-from django.urls import path
-
-from .views import DocumentApiRetrieve
-
-urlpatterns = [
-    path('documents/<pk>', DocumentApiRetrieve.as_view()),
-]
