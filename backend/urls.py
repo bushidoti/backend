59062ecbdb6081bbc357926ea1f545ef246b9296
sajjad <titanxl79@gmail.com>
2023-06-28 22:23:03 +0330

    test update

--- backend/urls.py
+++ backend/urls.py
@@ -53,4 +53,6 @@ urlpatterns = [
          name='token_refresh'),
     path('', include('authentification.urls')),
     path('api/', include(router.urls)),
-]
+] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+
+urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
